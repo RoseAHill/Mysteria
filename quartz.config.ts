e72ae25972cbad6cc8407ccff59291f4ba43784d
RoseAHill <roseaahill@gmail.com>
2024-05-22 18:15:36 -0400

    Quartz sync: May 22, 2024, 6:15 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
     },
     locale: "en-US",
     baseUrl: "quartz.jzhao.xyz",
-    ignorePatterns: ["private", "templates", ".obsidian"],
+    ignorePatterns: ["private", "templates", "Templates", ".obsidian", "meta", "Meta"],
     defaultDateType: "modified",
     theme: {
       fontOrigin: "googleFonts",
